c365aef51f031951178187ea06ef9de5814e55eb
zoha <mumtazzoha@gmail.com>
2021-04-26 18:15:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+## Description
+My portfolio created with the help of React.js
+the contents of my portfolio are as follows 
+about me 
+portfolio
+contact info
+and my resume
+it contains list of my skills and my porjects list
+at the bottom of the page u can see my github, linked in and skype.
+
+
+# Screen shot
+
+# User Story
+AS AN employer looking for candidates with experience building single-page applications
+I WANT to view a potential employee's deployed React portfolio of work samples
+SO THAT I can assess whether they're a good candidate for an open position
+
+
+## Installation 
+run the following commands 
+npm install -g create-react-app
+create-react-app portfolio
+cd portfolio
+npm start
+
+# License 
+Licesned under MIT 
+
+# Contribtions
+React.js and open source media and the help of U of t.
---
+++ src/resumeData.js
@@ -0,0 +1,104 @@
+let resumeData = {
+  
+  "name": "Zoha Mumtaz",
+  "role": "Frontend Developer",
+  "linkedinId":"Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I have done few interesting projects. I like to learn new things and have keen eye for design and creativity.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"I love exploring and learning about new things, places  I like to travel to a lot. I am passionate about achieving my goals.",
+  "address":"Canada",
+  
+  "education":[
+    {
+      "UniversityName":"University Of Toronto (UOFT)",
+      "specialization":"Full Stack Development",
+      "MonthOfPassing":"May",
+      "YearOfPassing":"2021",
+      "Achievements":"Certificate"
+    },
+    
+  ],
+  "work":[
+    {
+      "CompanyName":"IBM",
+      "specialization":"SQA",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Certificate"
+    },
+    
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"JS"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"budget-tracker",
+      "imgurl":"images/portfolio/budget-tracker.png"
+    },
+    {
+      "name":"project2",
+      "description":"code-quiz",
+      "imgurl":"images/portfolio/code-quiz.png"
+    },
+    {
+      "name":"project3",
+      "description":"horeseaon",
+      "imgurl":"images/portfolio/horeseaon.png"
+    },
+    {
+      "name":"project4",
+      "description":"note-taker",
+      "imgurl":"images/portfolio/note-taker.png"
+    },
+    {
+      "name":"project5",
+      "description":"password-generator",
+      "imgurl":"images/portfolio/password-generator.png"
+    },
+    {
+      "name":"project6",
+      "description":"weather-dashboard",
+      "imgurl":"images/portfolio/weather-dashboard.png"
+    },
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
